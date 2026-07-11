6ad55a02d5e041452a4f955b7a54929ac9bce298
phanuwat-ht <phanuwat@huaytad.ac.th>
2026-07-11 18:09:38 +0700

    Update GAS deployment URL (v24)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // ===========================================================================
 
 const LIFF_ID = "2010662195-iJjI0NIA";
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzLPEUR7RxO7JHHCa3Fq9qT_D2AXyawX07oaoSzN92HXJyJt1rkKgeOY3f5lbabsfpU8Q/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyLVl0lC5vRKzW15wIaYgKRjY5hntv-mwDQ5vEi7jcHkc-v9xzbvCFtq54rpsi0Iivuqg/exec";
 // ใส่ URL ของ logo รร. (Google Drive: เปลี่ยน /file/d/ID/view เป็น /uc?id=ID) หรือ URL รูปภาพอื่น
 // ถ้าว่าง = แสดงข้อความ "รร" เหมือนเดิม
 const SCHOOL_LOGO_URL = 'https://img2.pic.in.th/logo-ht.png';
